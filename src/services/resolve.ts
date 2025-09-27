127bef8a5776fbbc267974b034f1198ea60100df
Vedant Mahadik <mahadikvedant32@gmail.com>
2025-09-28 02:44:25 +0530

    ft: add 1inch API to execute orders

--- src/services/resolve.ts
+++ src/services/resolve.ts
@@ -1,5 +1,21 @@
 import { useExecutionDbClient } from "../utils";
 
+import {
+  Sdk,
+  MakerTraits,
+  Address,
+  randBigInt,
+  FetchProviderConnector,
+} from "@1inch/limit-order-sdk";
+
+import { Wallet } from "ethers";
+import { Execution } from "../models";
+
+const privKey = process.env.PRIVATE_KEY ?? "";
+const maker = new Wallet(privKey);
+
+const makerTraits = MakerTraits.default();
+
 export const resolveOrderService = async () => {
   const response = await useExecutionDbClient.getOpenExecutions();
   if (response.status != 200) {
@@ -8,10 +24,39 @@ export const resolveOrderService = async () => {
   const numExecToExecute = response.data.length;
   for (let idx = 0; idx < numExecToExecute; idx++) {
     const execution = response.data[idx];
-    // TODO: send execution to 1inch API
+    // @ts-ignore
+    await placeOrderForExecution(execution);
   }
   return {
     status: 200,
     data: "Resolved orders.",
   };
+};
+
+const placeOrderForExecution = async (execution: Execution) => {
+  const sdk = new Sdk({
+    authKey: process.env.ONE_INCH_API_KEY ?? "",
+    networkId: execution.chain_id,
+    httpConnector: new FetchProviderConnector(),
+  });
+
+  const order = await sdk.createOrder(
+    {
+      makerAsset: new Address(execution.currency_0),
+      takerAsset: new Address(execution.currency_1),
+      makingAmount: BigInt(execution.amount_0),
+      takingAmount: BigInt(execution.amount_1),
+      maker: new Address(execution.address),
+    },
+    makerTraits,
+  );
+
+  const typedData = order.getTypedData(execution.chain_id);
+  const signature = await maker.signTypedData(
+    typedData.domain,
+    { Order: typedData.types.Order },
+    typedData.message,
+  );
+
+  await sdk.submitOrder(order, signature);
 };
