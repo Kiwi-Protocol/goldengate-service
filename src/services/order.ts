4260c16896ded104d37a95634aed367579547db1
bamblebam <burhanizrangwala@gmail.com>
2025-09-27 09:45:34 +0530

    added typedData

--- src/services/order.ts
+++ src/services/order.ts
@@ -1,6 +1,28 @@
+import {
+  EIP712Object,
+  EIP712Types,
+  EIP712DomainType,
+} from "@1inch/limit-order-sdk";
 import { Order } from "../models";
-export const createNewOrderService = (order: Order) => {
+export const createNewOrderService = (order: Order, signature: String) => {
   console.log(order);
+  const expectedSignerAddress = order.fromAddress; // The address expected to have signed
+
+  const isValid = await verifyEIP712Signature(
+    {
+      domain: typedData.domain,
+      types: { Order: typedData.types.Order },
+      message: typedData.message,
+    },
+    signature,
+    expectedSignerAddress
+  );
+
+  if (isValid) {
+    console.log("EIP-712 signature is valid!");
+  } else {
+    console.log("EIP-712 signature is invalid.");
+  }
 };
 
 export const getPendingOrdersService = () => {
